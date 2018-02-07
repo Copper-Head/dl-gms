2714dd56ba6e74553d96629f3388b0cc8f298598
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2018-02-07 10:34:05 +0100

    fixed bad path

--- docker_train.sh
+++ docker_train.sh
@@ -7,7 +7,7 @@ source <(grep = config.ini)
 NV_GPU=$GPUs nvidia-docker run -d \
           --name=$name \
           -l "lego-vae=true" \
-          -v /srv/data/lgm2017-lego/orange_legos:$data_dir \
+          -v /srv/data/lgm2017-lego/lego_orange:$data_dir \
           -v $(pwd):/scripts \
           -v /srv/data/lgm2017-lego/weights:$weights_dir \
           -v /srv/data/lgm2017-lego/tensorboard-logdir:$tb_logdir \
